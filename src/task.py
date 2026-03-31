f7f0bed0f485bb89028195e12a4ffbb3aaa4388d
ISZenzi <zenzirovi@gmail.com>
2026-04-01 00:19:17 +0300

    add docstings

--- src/task.py
+++ src/task.py
@@ -15,12 +15,23 @@ class TaskStatus(Enum):
 
 
 class Task:
+    """
+    Модель задачи
+    """
+
     priority = PriorityDescriptor(min_val=1, max_val=5)
     description = DefaultDescriptionDescriptor()
 
     def __init__(
         self, id: int, payload: Any, priority: int = 3, description: str = ""
     ) -> None:
+        """
+        Инициализирует новую задачу
+        :param id: Идентификатор задачи
+        :param payload: Полезная нагрузка задачи
+        :param priority: Приоритет задачи
+        :param description: Описание задачи
+        """
         self._id = id
         self.payload = payload
         self.priority = priority
@@ -32,21 +43,40 @@ class Task:
 
     @property
     def id(self) -> int:
+        """
+        Возвращает идентификатор задачи
+        :return: Идентификатор задачи
+        """
         return self._id
 
     @property
     def created_at(self) -> datetime:
+        """
+        Возвращает время создания задачи
+        :return: Отметка времени
+        """
         return self._created_at
 
     @property
     def status(self) -> TaskStatus:
+        """
+        Возвращает текущий статус задачи
+        :return: Статус задачи
+        """
         return self._status
 
     @property
     def ready_for_execution(self) -> bool:
+        """
+        Проверяет, готова ли задача к выполнению
+        :return: True, если статус задачи NEW, иначе False
+        """
         return self.status == TaskStatus.NEW
 
     def start(self) -> None:
+        """
+        Переводит задачу в статус IN_PROGRESS
+        """
         if self._status != TaskStatus.NEW:
             raise InvalidStateError(
                 f"Нельзя запустить задачу в статусе {self._status.value}"
@@ -54,11 +84,17 @@ class Task:
         self._status = TaskStatus.IN_PROGRESS
 
     def complete(self) -> None:
+        """
+        Переводит задачу в статус COMPLETED
+        """
         if self._status != TaskStatus.IN_PROGRESS:
             raise InvalidStateError("Завершить можно только начатую задачу")
         self._status = TaskStatus.COMPLETED
 
     def fail(self) -> None:
+        """
+        Переводит задачу в статус FAILED
+        """
         if self._status not in (TaskStatus.IN_PROGRESS, TaskStatus.NEW):
             raise InvalidStateError(
                 "Завершить с ошибкой можно только начатую или новую задачу"
@@ -66,6 +102,10 @@ class Task:
         self._status = TaskStatus.FAILED
 
     def __repr__(self):
+        """
+        Возвращает строковое представление задачи
+        :return: Строка с представлением
+        """
         return (
             f"Task(id={self.id}, priority={self.priority}, status={self.status.value})"
         )
